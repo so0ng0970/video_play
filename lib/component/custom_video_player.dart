760ce9375f3374723f7ed0db04fab6e25de4a5c6
soonger3306 <soonger3306@daum.net>
2023-04-02 17:39:39 +0900

    play,pause,reverse,forward

--- lib/component/custom_video_player.dart
+++ lib/component/custom_video_player.dart
@@ -6,6 +6,7 @@ import 'package:video_player/video_player.dart';
 
 class CustomVideoPlayer extends StatefulWidget {
   final XFile video;
+
   const CustomVideoPlayer({required this.video, super.key});
 
   @override
@@ -14,6 +15,7 @@ class CustomVideoPlayer extends StatefulWidget {
 
 class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
   VideoPlayerController? videoController;
+
   @override
   void initState() {
     // TODO: implement initState
@@ -36,8 +38,125 @@ class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
     if (videoController == null) {
       return const CircularProgressIndicator();
     }
-    return VideoPlayer(
-      videoController!,
+
+    // 원래 비율대로 설정할려면 AspectRatio
+    return Center(
+      child: AspectRatio(
+        aspectRatio: videoController!.value.aspectRatio,
+        child: Stack(
+          children: [
+            VideoPlayer(
+              videoController!,
+            ),
+            _Controls(
+              onReversePressed: onReversePressed,
+              onPlayPressed: onPlayPressed,
+              isPlay: videoController!.value.isPlaying,
+              onForwardPressed: onForwardPressed,
+            ),
+            // stack 쓸때 많이 씀 위치지정
+            Positioned(
+              right: 1,
+              child: IconButton(
+                onPressed: () {},
+                color: Colors.white,
+                iconSize: 30.0,
+                icon: const Icon(Icons.photo_camera_back),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  void onReversePressed() {
+    final currentPosition = videoController!.value.position;
+
+    Duration position = const Duration(); // 0초
+
+    if (currentPosition.inSeconds > 2) {
+      position = currentPosition - const Duration(seconds: 2);
+    }
+
+    videoController!.seekTo(position);
+  }
+
+  void onPlayPressed() {
+    // 이미 실행중이면 중지
+    // 실행중이면 중지
+
+    setState(() {
+      if (videoController!.value.isPlaying) {
+        videoController!.pause();
+      } else {
+        videoController!.play();
+      }
+    });
+  }
+
+  void onForwardPressed() {
+    // 전체 총길이
+    final maxPosition = videoController!.value.duration;
+    final currentPosition = videoController!.value.position;
+    Duration position = maxPosition;
+    if ((maxPosition - const Duration(seconds: 2)).inSeconds >
+        currentPosition.inSeconds) {
+      position = currentPosition + const Duration(seconds: 2);
+    }
+
+    videoController!.seekTo(position);
+  }
+}
+
+class _Controls extends StatelessWidget {
+  final VoidCallback onPlayPressed;
+  final VoidCallback onReversePressed;
+  final VoidCallback onForwardPressed;
+  final bool isPlay;
+
+  const _Controls({
+    required this.onPlayPressed,
+    required this.onReversePressed,
+    required this.onForwardPressed,
+    required this.isPlay,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black.withOpacity(0.5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          renderIconButton(
+            onPressed: onReversePressed,
+            iconData: Icons.rotate_left,
+          ),
+          renderIconButton(
+            onPressed: onPlayPressed,
+            iconData: isPlay ? Icons.pause : Icons.play_arrow,
+          ),
+          renderIconButton(
+            onPressed: onForwardPressed,
+            iconData: Icons.rotate_right,
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget renderIconButton({
+    required VoidCallback onPressed,
+    required IconData iconData,
+  }) {
+    return IconButton(
+      onPressed: onPressed,
+      iconSize: 30.0,
+      color: Colors.white,
+      icon: Icon(iconData),
     );
   }
 }
